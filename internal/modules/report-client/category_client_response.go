d01ba23df703a78a600edc14d717aaa77c403950
Artt Vincii <otong123321@gmail.com>
2026-07-31 23:30:31 +0700

    feat(report-client): expose category lookup methods

---
+++ internal/modules/report-client/category_client_response.go
@@ -0,0 +1,7 @@
+package report_client
+
+type CategoryClientResponse struct {
+	ID   string
+	Name string
+	Slug string
+}
--- internal/modules/report-client/client.go
+++ internal/modules/report-client/client.go
@@ -1,12 +1,9 @@
 package report_client
 
-import (
-	"gorm.io/gorm"
-)
-
-
-
+import "gorm.io/gorm"
 
 type Client interface {
 	CreateReport(tx *gorm.DB, req *ReportClientRequest) (*ReportClientResponse, error)
+	GetAllCategories(tx *gorm.DB) ([]CategoryClientResponse, error)
+	GetCategoryBySlug(tx *gorm.DB, slug string) (*CategoryClientResponse, error)
 }
