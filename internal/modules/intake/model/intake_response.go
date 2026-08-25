12729a4d26d2acd92869d91227f5c182acd190c4
Artt Vincii <otong123321@gmail.com>
2026-08-25 14:00:34 +0700

    refactor: hapus modul intake, pindahkan fungsionalitas ke report module

--- internal/modules/intake/model/intake_response.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type IssueAnalysisResultResponse struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Category    string `json:"category"`
-	Severity    string `json:"severity_level"`
-}
